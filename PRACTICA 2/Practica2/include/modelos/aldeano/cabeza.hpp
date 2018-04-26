4f90111d44c2871b31a77eaa4a4d8fa9c8de5441
Cecilia <ceci.merelo@gmail.com>
2018-04-26 06:01:20 +0200

    Inicio del proyecto

---
+++ PRACTICA 2/Practica2/include/modelos/aldeano/cabeza.hpp
@@ -0,0 +1,17 @@
+#ifndef CABEZAAldeano3D_HPP
+#define CABEZAAldeano3D_HPP
+
+#include "obj3dlib.hpp"
+#include <string.h>
+
+
+class CabezaAldeano3D : public Objeto3D{
+private:
+
+public:
+  CabezaAldeano3D(const std::string & filename);
+  ~CabezaAldeano3D();
+
+};
+
+#endif
