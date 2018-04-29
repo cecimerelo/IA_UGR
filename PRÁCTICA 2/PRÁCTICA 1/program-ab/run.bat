9b2aa6959f31f39c5754a520fafc9e667ca9be6d
Cecilia <ceci.merelo@gmail.com>
2018-04-29 20:12:31 +0200

    Terminado nivel 2

--- PRÁCTICA 2/PRÁCTICA 1/program-ab/run.bat
+++
@@ -1,4 +0,0 @@
-java -cp lib/Ab.jar Main bot=mybot action=chat trace=false morph=false
-
-
-
--- PRÁCTICA 2/PRÁCTICA 1/program-ab/run.sh
+++
@@ -1,3 +0,0 @@
-# Run bot
-rm -fr bots/mybot/aimlif/*
-java -cp lib/Ab.jar Main bot=mybot action=chat trace=false morph=false
